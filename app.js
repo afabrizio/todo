2e1908df906ee826c73d30870a73e3ecb97fe0e5
afabrizio <andrew.m.fabrizio@gmail.com>
2016-10-19 16:48:51 -0700

    App is able to successfully get tasks from the mongo database.

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const express = require('express');
-const mongodb = require('mongodb');
-const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 app.use(express.static('public'));
-app.use(bodyParser.json());
 
 app.get('/todos', (req, res) => {
-  res.send('reqeust was handled');
+  getTasks(res);
 });
 
 app.listen(3300, () => {
   console.log('express server listening on port 3300!');
 });
 
-const { MongoClient } = mongodb;
-MongoClient.connect('mongodb://localhost:27017/todo-app'), (err, db) => {
-  if(err) {
-    console.error(err);
-    process.exit(1);
-  }
+function getTasks(res) {
+  MongoClient.connect('mongodb://localhost:27017/todo-app', (err, db) => {
+    if(err) {
+      console.error(err);
+      process.exit(1);
+    }
 
-  const todos = db.collection('todos').find();
-  console.log(todos);
+    db.collection('todos').find({}).toArray( (err, documents) => {
+      db.close();
+      res.json(documents);
+    });
+  });
 }
--- public/default.js
+++ public/default.js
@@ -8,33 +8,26 @@ todos.controller('HomeController', HomeController);
 HomeController.$inject = ['$http'];
 
 function HomeController($http) {
-  const todos = [
-    {description: 'Get groceries.', complete: false},
-    {description: 'Do dishes.', complete: false},
-    {description: 'Fold laundry', complete: false}
-  ]
-  this.todos = loadTodos();
-  this.toggleComplete = todo => {
-    todo.complete = !todo.complete;
-    this.remaining = getRemaining(this.todos);
+  let vm=this;
+
+  $http.get('/todos')
+    .then(successCallback, errorCallback)
+    .catch(err => alert(err.message))
+
+  function successCallback(response) {
+    vm.todos = response.data;
+    vm.toggleComplete = todo => {
+      todo.complete = !todo.complete;
+      vm.remaining = getRemaining(vm.todos);
+    }
+    vm.remaining = getRemaining(vm.todos);
+
+    function getRemaining(todos) {
+      return todos.filter(todo => !todo.complete).length;
+    }
   }
 
-  this.remaining = getRemaining(this.todos);
-
-  function getRemaining(todos) {
-    return todos.filter(todo => !todo.complete).length;
-  }
-
-  function loadTodos() {
-    var loadedTodos = [];
-    $http.get('/todos').then(successCallback, errorCallback);
-
-    function successCallback(response) {
-      loadedTodos = response.data;
-    }
-    function errorCallback(err) {
-      console.error(err.data);
-    }
-    return loadedTodos;
+  function errorCallback(err) {
+    console.error(err);
   }
 }
