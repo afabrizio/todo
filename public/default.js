b901ac325c650f1ce33902d8db275aa8f4490d94
afabrizio <andrew.m.fabrizio@gmail.com>
2016-10-21 11:52:02 -0700

    Refactored default.js to now use an Angular factory for HTTP requests.

--- public/default.js
+++ public/default.js
@@ -1,47 +1,51 @@
 const todos = angular.module('todos', []);
 
+//HTTP factory:
+todos.factory('todosData', ['$http', function ($http) {
+  return {
+    retrieveTodos,
+    createTodo
+  }
+
+  function retrieveTodos($http) {
+    return $http.get('/todos');
+  }
+
+  function createTodo($http, newTodo) {
+    return $http.post('/todos', {task: newTodo, complete: false} );
+  }
+
+}])
+
+//Greet Controller:
 todos.controller('Greet', function() {
   this.message = "Hello";
 })
 
-todos.controller('HomeController', HomeController);
-HomeController.$inject = ['$http'];
-
-function HomeController($http) {
+//HomeController:
+todos.controller('HomeController', ['$http', 'todosData', function ($http, todosData) {
   var vm = this;
-
-  $http.get('/todos')
-    .then(successCallback, errorCallback)
-    .catch(err => alert(err.message));
-
-  vm.addTodo = () => {
-    $http.post('/todos', {task: vm.newTodo, complete: false} )
-      .then( () => {
-        updateView();
-      })
-  }
-
-  function successCallback(response) {
-    vm.todos = response.data;
-    vm.toggleComplete = todo => {
-      todo.complete = !todo.complete;
-      vm.remaining = getRemaining(vm.todos);
-    }
+  todosData.retrieveTodos($http).then( res => {
+    vm.todos = res.data;
     vm.remaining = getRemaining(vm.todos);
-
-    function getRemaining(todos) {
-      return todos.filter(todo => !todo.complete).length;
-    }
+  });
+  vm.toggleComplete = todo => {
+    todo.complete = !todo.complete;
+    vm.remaining = getRemaining(vm.todos);
+  }
+  vm.addTodo = () => {
+    todosData.createTodo($http, vm.newTodo)
+      .then( () => {
+          todosData.retrieveTodos($http).then( res => {
+            vm.todos = res.data;
+            vm.remaining = getRemaining(vm.todos);
+            document.getElementById('newTodo').value = '';
+          })
+      });
   }
 
-  function errorCallback(err) {
-    console.error(err);
+  function getRemaining(todos) {
+    return todos.filter(todo => !todo.complete).length;
   }
 
-  function updateView() {
-    $http.get('/todos')
-      .then(successCallback, errorCallback)
-      .catch(err => alert(err.message));
-    document.getElementById('newTodo').value = '';
-  }
-}
+}]);
